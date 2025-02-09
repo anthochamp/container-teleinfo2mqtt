3af7a160f10933eb6d4bdc37a2c2e9926654e7cd
Anthony Champagne <dev@anthonychampagne.fr>
2025-02-09 01:55:17 +0100

    docker-entrypoint: lint

--- src/rootfs/docker-entrypoint.sh
+++ src/rootfs/docker-entrypoint.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env sh
 set -eu
 
-# shellcheck disable=SC2120
+# shellcheck disable=SC2120,SC3043
 replaceEnvSecrets() {
-  # replaceEnvSecrets 1.0.0
-  local prefix="${1:-}"
+	# replaceEnvSecrets 1.0.0
+	# https://gist.github.com/anthochamp/d4d9537f52e5b6c42f0866dd823a605f
+	local prefix="${1:-}"
 
-  for envSecretName in $(export | awk '{print $2}' | grep -oE '^[^=]+' | grep '__FILE$'); do
-    if [ -z "$prefix" ] || printf '%s' "$envSecretName" | grep "^$prefix" >/dev/null; then
-      local envName
-      envName=$(printf '%s' "$envSecretName" | sed 's/__FILE$//')
+	for envSecretName in $(export | awk '{print $2}' | grep -oE '^[^=]+' | grep '__FILE$'); do
+		if [ -z "$prefix" ] || printf '%s' "$envSecretName" | grep "^$prefix" >/dev/null; then
+			local envName
+			envName=$(printf '%s' "$envSecretName" | sed 's/__FILE$//')
 
-      local filePath
-      filePath=$(eval echo '${'"$envSecretName"':-}')
+			local filePath
+			filePath=$(eval echo '${'"$envSecretName"':-}')
 
-      if [ -n "$filePath" ]; then
-        if [ -f "$filePath" ]; then
-          echo Using content from "$filePath" file for "$envName" environment variable value.
+			if [ -n "$filePath" ]; then
+				if [ -f "$filePath" ]; then
+					echo Using content from "$filePath" file for "$envName" environment variable value.
 
-          export "$envName"="$(cat -A "$filePath")"
-          unset "$envSecretName"
-        else
-          echo ERROR: Environment variable "$envSecretName" is defined but does not point to a regular file. 1>&2
-          exit 1
-        fi
-      fi
-    fi
-  done
+					export "$envName"="$(cat -A "$filePath")"
+					unset "$envSecretName"
+				else
+					echo ERROR: Environment variable "$envSecretName" is defined but does not point to a regular file. 1>&2
+					exit 1
+				fi
+			fi
+		fi
+	done
 }
 
 replaceEnvSecrets
